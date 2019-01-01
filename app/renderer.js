fb7f550ca2ea3844b04bb574ce026c14ec1fe1ea
Gordon Guthrie <gordon@hypernumbers.com>
2019-01-01 23:43:32 +0100

    Big restructuring of code:
    * fix up a custom menu structure
    * create IPC between the main and the rendering
    process
    * start breaking up the html in smaller components
    
    This set of tasks start the process of breaking the
    development of individual screens into their own
    little world (and also the javascript for each
    component)

--- app/renderer.js
+++ app/renderer.js
@@ -2,12 +2,13 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const remote = require('electron').remote; 
+const remote = require('electron').remote;
 const app = require('electron').remote.app;
 const dialog = require('electron').remote.dialog;
 const fs = require('fs');
 const song = require('brill-song');
 const settings = require('electron-settings');
+const ipcRenderer = require('electron').ipcRenderer;
 
 // global variables
 var currentsong;
@@ -37,6 +38,15 @@ var init = function () {
     };
 };
 
+var is_empty = function (obj) {
+    for (var key in obj) {
+	if(obj.hasOwnProperty(key)) {
+	    return false;
+	};
+    };
+    return true;
+};
+
 var get_songwriters_from_settings = function () {
     if (settings.has('songwriters')) {
 	return settings.get('songwriters');
@@ -54,18 +64,28 @@ var get_songs_dir_from_settings = function () {
     }
 };
 
+var get_array_value = function(route) {
+    var routes = route.split(":");
+    return currentsong.arrayGet(routes[0], routes[1], routes[2]);
+};
+
 var get_value = function(route) {
     var routes = route.split(":");
     return currentsong.get(routes[0], routes[1]);
 };
 
+var get_whole_array = function(route) {
+    var routes = route.split(":");
+    return currentsong.getWholeArray(routes[0], routes[1]);
+};
+
 var update_data = function(val, route) {
-    var routes = route.split(":");  
+    var routes = route.split(":");
     currentsong.set(val, routes[0], routes[1]);
     maybe_compile();
     render();
 };
-    
+
 var render = function() {
     var values = $("input[data-route]");
     values.each(function (d) {
@@ -80,7 +100,19 @@ var render = function() {
 };
 
 var render_swing = function () {
-    console.log("rendering swing");
+    var swing = get_whole_array("swing:swing");
+    var is_obj_empty = is_empty(swing);
+    if (is_empty) {
+	var no_of_notes = parseInt(get_value("timing:beats_to_the_bar"), 10);
+	for (var i = 0; i < no_of_notes; i++) {
+	    var beatname = "beat" + (i + 1);
+	    currentsong.add("swing", beatname);
+	    currentsong.arraySet(0, "swing", beatname, "swing");
+	    currentsong.arraySet(0, "swing", beatname, "emphasis");
+	};
+    } else {
+	console.log("got swing");
+    };
 };
 
 var maybe_compile = function () {
@@ -88,13 +120,6 @@ var maybe_compile = function () {
     if ($("#brill-compile-setting").is(":checked")) {
 	currentsong.compile();
     };
-};
-
-var show_hidden = function () {
-};
-
-var show_song = function () {
-    $("[data-hidden='song']").transition('fade');
 };
 
 var is_type_valid = function(val, type) {
@@ -110,9 +135,7 @@ var is_type_valid = function(val, type) {
 };
 
 var get_fieldname = function(classname) {
-    console.log("classname is " + classname);
     var segs = classname.split("-");
-    console.log("segs is" + segs);
     return segs[1];
 };
 
@@ -130,7 +153,7 @@ var on_change = function (e) {
 	    " which is not of type " + type;
 	$(".brill-error").text(msg);
 	$(".brill-error-msg").removeClass("hidden");
-	$(".brill-error-msg").addClass("visible");	
+	$(".brill-error-msg").addClass("visible");
     }
 };
 
@@ -150,50 +173,24 @@ $('.tabular.menu .item').tab();
 // semantic ui dropdowns
 //$(".brill-time-signature").dropdown('setting', 'onChange', on_select_change);
 
+//
+// bind brill gui controls
+//
+
+//
+// handle menu updates
+//
+var enable_menu = function (id) {
+    console.log("about to send an aync message");
+    var payload = new Object();
+    payload.type = "enable menu";
+    payload.menu_id = id;
+    console.log(payload);
+    ipcRenderer.send('asynchronous-message', payload);
+};
+
 // bind data fields to update the song
 $("input[data-route]").change(on_change);
-
-// bind buttons and stuff
-$('.ui.button.brill-open').click(function () {
-    var openFn = function (fileName) {
-	if(fileName !== undefined){
-	    var writers = get_songwriters_from_settings();
-	    currentsong = song.open(fileName[0], writers);
-	    show_song();
-	    render();
-	    $('.ui.modal.brillopen').modal('hide');
-	}
-    };
-    var songs_dir = get_songs_dir_from_settings();
-    console.log("songs dir is " + songs_dir);
-    var properties = {properties: ['openDirectory', 'CreateDirectory'],
-		      defaultPath: songs_dir};
-    dialog.showOpenDialog(properties, openFn);
-});
-    
-$('.ui.button.brill-new').click(function () {
-    var home = app.getPath('home');
-    var openFn = function (fileName) {
-	if(fileName !== undefined){
-	    var selectedsong = fileName;
-	    fs.mkdir(selectedsong, function(err){
-		if (err) {
-		    return console.error(err);
-		}
-		var writers = get_songwriters_from_settings();
-		currentsong = song.open(selectedsong, writers);
-		show_song();
-		render(); 
-		$('.ui.modal.brillopen').modal('hide');		
-	    });
-	}
-    };
-    var songs_dir = get_songs_dir_from_settings();
-    console.log("songs dir is " + songs_dir);
-    var properties = {properties: ['openDirectory', 'CreateDirectory'],
-		      defaultPath: songs_dir};
-    dialog.showSaveDialog(properties, openFn);
-});
 
 $("input[data-settings").change(function (e) {
     var type = $(e.target).attr("data-type");
@@ -209,11 +206,70 @@ $("input[data-settings").change(function (e) {
 });
 
 //
+// functions to handle interprocess communications
+//
+ipcRenderer.on('new', function(ev, data) {
+    var home = app.getPath('home');
+    var openFn = function (fileName) {
+	if(fileName !== undefined){
+	    var selectedsong = fileName;
+	    fs.mkdir(selectedsong, function(err){
+		if (err) {
+		    return console.error(err);
+		}
+		var writers = get_songwriters_from_settings();
+		currentsong = song.open(selectedsong, writers);
+		console.log(currentsong);
+		enable_menu("brill-menu-title");
+		render();
+		$('.ui.modal.brillopen').modal('hide');
+	    });
+	}
+    };
+    var songs_dir = get_songs_dir_from_settings();
+    var properties = {properties: ['openDirectory', 'CreateDirectory'],
+		      defaultPath: songs_dir};
+    dialog.showSaveDialog(properties, openFn);
+});
+
+ipcRenderer.on('open', function(ev, data) {
+    var openFn = function (fileName) {
+	if(fileName !== undefined){
+	    var writers = get_songwriters_from_settings();
+	    currentsong = song.open(fileName[0], writers);
+	    enable_menu("brill-menu-title");
+	    render();
+	    $('.ui.modal.brillopen').modal('hide');
+	}
+    };
+    var songs_dir = get_songs_dir_from_settings();
+    var properties = {properties: ['openDirectory', 'CreateDirectory'],
+		      defaultPath: songs_dir};
+    dialog.showOpenDialog(properties, openFn);
+});
+
+ipcRenderer.on('settings', function(ev, data) {
+    console.log("in settings");
+});
+
+ipcRenderer.on('title', function(ev, data) {
+    $("#brill-main").load('./title.html');
+});
+
+ipcRenderer.on('timing', function(ev, data) {
+    console.log("in timing");
+});
+
+ipcRenderer.on('instruments', function(ev, data) {
+    console.log("in instruments");
+});
+
+
+//
 // now run init
 //
 init();
 
 currentsong = song.open("/home/vagrant/Songs/Evie", "Gordo");
-show_song();
+enable_menu("brill-menu-title");
 render();
-
--- main.js
+++ main.js
@@ -1,5 +1,148 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow, ipcMain} = require('electron')
+
+const menuTemplate = [
+    {
+	label: 'File',
+	submenu: [
+	    {
+		label: 'New Song',
+		accelerator: 'CmdOrCtrl+N',
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('new');
+		}
+	    },
+	    {
+		label: 'Open Song',
+		accelerator: 'CmdOrCtrl+O',
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('open');
+		}
+	    },
+	    { type: 'separator' },
+	    {
+		label: 'Settings',
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('settings');
+		}
+	    },
+	    {role: 'Quit'}
+	]
+    },
+    {
+	label: 'Song',
+	submenu: [
+	    {
+		label: 'Title and details',
+		id: 'brill-menu-title',
+		enabled: false,
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('title');
+		}
+	    },
+	    {
+		label: 'Timing',
+		id: 'brill-menu-timing',
+		enabled: false,
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('timing');
+		}
+	    },
+	    {
+		label: 'Instruments',
+		id: 'brill-menu-instruments',
+		enabled: false,
+		click: function (menuItem, currentWindow) {
+		    currentWindow.webContents.send('instruments');
+		}
+	    }
+	]
+    },
+    {
+	label: 'Edit',
+	submenu: [
+	    { role: 'undo' },
+	    { role: 'redo' },
+	    { type: 'separator' },
+	    { role: 'cut' },
+	    { role: 'copy' },
+	    { role: 'paste' },
+	    { role: 'pasteandmatchstyle' },
+	    { role: 'delete' },
+	    { role: 'selectall' }
+	]
+    },
+    {
+	label: 'View',
+	submenu: [
+	    { role: 'reload' },
+	    { role: 'forcereload' },
+	    { role: 'toggledevtools' },
+	    { type: 'separator' },
+	    { role: 'resetzoom' },
+	    { role: 'zoomin' },
+	    { role: 'zoomout' },
+	    { type: 'separator' },
+	    { role: 'togglefullscreen' }
+	]
+    },
+    {
+	role: 'window',
+	submenu: [
+	    { role: 'minimize' },
+	    { role: 'close' }
+	]
+    },
+    {
+	role: 'help',
+	submenu: [
+	    {
+		label: 'Learn More',
+		click () { require('electron').shell.openExternal('https://electronjs.org') }
+	    }
+	]
+    }
+];
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  })
+    // Edit menu
+    template[1].submenu.push(
+	{ type: 'separator' },
+	{
+	    label: 'Speech',
+	    submenu: [
+		{ role: 'startspeaking' },
+		{ role: 'stopspeaking' }
+	    ]
+	}
+    )
+
+    // Window menu
+    template[3].submenu = [
+	{ role: 'close' },
+	{ role: 'minimize' },
+	{ role: 'zoom' },
+	{ type: 'separator' },
+	{ role: 'front' }
+    ]
+}
+
+// declare the main menu as a global so we can interogate it later
+const mainMenu = Menu.buildFromTemplate(menuTemplate);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -11,10 +154,10 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('app/index.html')
-    
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
-    
+
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
@@ -29,6 +172,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
     createWindow();
+    Menu.setApplicationMenu(mainMenu);
 });
 
 // Quit when all windows are closed.
@@ -47,3 +191,10 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+ipcMain.on('asynchronous-message', function (event, payload) {
+    if (payload.type === "enable menu") {
+	var menuItem = mainMenu.getMenuItemById(payload.menu_id);
+	menuItem.enabled = true;
+    };
+});
